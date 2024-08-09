90990f751f259ed714612054169d21f8e0abbd68
1Mochiyuki <notsmile@protonmail.com>
2024-08-09 01:44:10 -0400

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	"github.com/1Mochiyuki/Catbox2Embed/fileupload"
+)
+
+func newUploadFileSection(container *fyne.Container, label *widget.Label) fyne.Widget {
+
+	if label == nil {
+		fmt.Println("label nil, using default label")
+		label = widget.NewLabel("No file selected")
+	}
+	uploadBtn := widget.NewButtonWithIcon("", theme.MoveUpIcon(), nil)
+	cancelBtn := widget.NewButtonWithIcon("", theme.ContentClearIcon(), nil)
+	openFileBtn := widget.NewButtonWithIcon("", theme.FolderNewIcon(), nil)
+
+	fileUploadWidget := fileupload.NewFileUploadWidget(container, uploadBtn, cancelBtn, openFileBtn, label)
+	return fileUploadWidget
+}
+
+type Instructions struct {
+	widget.BaseWidget
+	InstructionsText string
+	OnTapped         func()
+}
+
+func NewInstructions(instructionsText string, onTapped func()) *Instructions {
+	fmt.Println("here")
+
+	instructions := &Instructions{
+		InstructionsText: instructionsText,
+		OnTapped:         onTapped,
+	}
+	instructions.ExtendBaseWidget(instructions)
+	return instructions
+}
+
+func (i *Instructions) CreateRenderer() fyne.WidgetRenderer {
+	con := container.NewCenter(widget.NewLabelWithStyle(i.InstructionsText, fyne.TextAlignCenter, widget.RichTextStyleHeading.TextStyle))
+
+	return widget.NewSimpleRenderer(con)
+}
+func (i *Instructions) Tapped(ev *fyne.PointEvent) {
+	fmt.Println("tapped")
+	i.OnTapped()
+}
+
+func main() {
+
+	a := app.NewWithID("Catbox2Embed")
+	window := a.NewWindow("Catbox2Embed")
+	window.Resize(fyne.NewSize(600, 500))
+	mainContainer := container.NewVBox()
+	uploadAllBtn := widget.NewButton("Upload All", func() {})
+	uploadAllBtn.OnTapped = func() {
+		for _, v := range mainContainer.Objects {
+
+			if reflect.TypeOf(v) == reflect.TypeOf(&fileupload.FileUploadWidget{}) {
+				uploadWidget := v.(*fileupload.FileUploadWidget)
+
+				uploadWidget.StartUploadButton.OnTapped()
+			}
+		}
+	}
+	clearAllBtn := widget.NewButton("Clear All", func() {})
+
+	clearAllBtn.OnTapped = func() {
+		newSlice := mainContainer.Objects[1:]
+		fmt.Printf("len of mainContainer obj: %v\n", len(mainContainer.Objects))
+		fmt.Printf("len of new slice: %v\n", len(newSlice))
+		for i, v := range newSlice {
+			fmt.Printf("obj of %v @ : %v\n", reflect.TypeOf(v), i+1)
+			fmt.Printf("len of main container obj before deletion: %v\n", len(mainContainer.Objects))
+			mainContainer.Remove(mainContainer.Objects[len(mainContainer.Objects)-1])
+			fmt.Printf("len of main container obj after deletion: %v\n", len(mainContainer.Objects))
+			fmt.Println("----------------------")
+		}
+		mainContainer.Add(newUploadFileSection(mainContainer, nil))
+	}
+
+	hbox := container.NewAdaptiveGrid(2, uploadAllBtn, clearAllBtn)
+
+	mainContainer.Add(hbox)
+	fmt.Printf("len of mainContainer obj: %v\n", len(mainContainer.Objects))
+
+	window.SetOnDropped(func(p fyne.Position, u []fyne.URI) {
+		window.SetContent(mainContainer)
+		for _, v := range u {
+			fileInfo, err := os.Stat(v.Path())
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("file: %s\nsize of file: %v MiB\n", v.Path(), (fileInfo.Size()/1024)/1024)
+
+			uploadWidget := newUploadFileSection(mainContainer, widget.NewLabel(v.Path()))
+			mainContainer.Add(uploadWidget)
+		}
+		if len(mainContainer.Objects) > 1 {
+			mainContainer.Add(newUploadFileSection(mainContainer, nil))
+		}
+
+	})
+
+	if len(mainContainer.Objects) >= 0 {
+		fmt.Println("nothing in ui, adding instructions")
+
+		instructions := NewInstructions("Click or drag files to begin", func() {
+			fileUploadSection := newUploadFileSection(mainContainer, nil)
+			mainContainer.Add(fileUploadSection)
+			window.SetContent(mainContainer)
+		})
+
+		window.SetContent(instructions)
+	}
+
+	window.ShowAndRun()
+}
---
+++ fileupload/fileupload.go
@@ -0,0 +1,81 @@
+package fileupload
+
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/sqweek/dialog"
+)
+
+type FileUploadWidget struct {
+	widget.BaseWidget
+	OpenFileButton     *widget.Button
+	StartUploadButton  *widget.Button
+	CancelUploadButton *widget.Button
+	FileName           *widget.Label
+}
+
+var DEFAULT_LABEL_TEXT = "No file selected"
+
+func NewFileUploadWidget(container *fyne.Container, startUploadButton, cancelUploadButton, openFileButton *widget.Button, fileName *widget.Label) *FileUploadWidget {
+
+	item := &FileUploadWidget{
+		OpenFileButton:     openFileButton,
+		StartUploadButton:  startUploadButton,
+		CancelUploadButton: cancelUploadButton,
+		FileName:           fileName,
+	}
+
+	item.FileName.Alignment = fyne.TextAlignCenter
+
+	cancelUploadButton.OnTapped = func() {
+		fileName.SetText(DEFAULT_LABEL_TEXT)
+		if len(container.Objects) > 1 { 
+		fmt.Printf("length of container objects: %v\n", len(container.Objects))
+		objPlace := len(container.Objects) - 1
+			container.Remove(container.Objects[objPlace])
+			return
+		}
+		fmt.Println("file name cleared, nothing else happened")
+	}
+
+	startUploadButton.OnTapped = func() {
+		if fileName.Text == DEFAULT_LABEL_TEXT {
+			fmt.Println("no file selected, not uploading")
+			return
+		}
+		filePath := fileName.Text
+		fmt.Printf("uploading: %s\n", filePath)
+		/* if url, err := catbox.New(nil).Upload(filePath); err != nil {
+			fmt.Printf("\ncatbox: %v\n", err)
+			return
+		} else {
+			fmt.Printf("\nurl: %s\npath: %s", url, filePath)
+		} */
+	}
+
+	openFileButton.OnTapped = func() {
+		fmt.Println("set open file tapped func")
+		filename, err := dialog.File().Load()
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+			return
+		}
+		fileName.SetText(filename)
+
+		nextUploadBtn := widget.NewButtonWithIcon(startUploadButton.Text, startUploadButton.Icon, startUploadButton.OnTapped)
+		nextCancelBtn := widget.NewButtonWithIcon(cancelUploadButton.Text, cancelUploadButton.Icon, cancelUploadButton.OnTapped)
+		nextOpenFileBtn := widget.NewButtonWithIcon(openFileButton.Text, openFileButton.Icon, openFileButton.OnTapped)
+
+		container.Add(NewFileUploadWidget(container, nextUploadBtn, nextCancelBtn, nextOpenFileBtn, widget.NewLabel(DEFAULT_LABEL_TEXT)))
+	}
+	item.ExtendBaseWidget(item)
+	return item
+}
+
+func (item *FileUploadWidget) CreateRenderer() fyne.WidgetRenderer {
+	c := container.NewHBox(item.CancelUploadButton, item.StartUploadButton, item.OpenFileButton, item.FileName)
+	return widget.NewSimpleRenderer(c)
+}
